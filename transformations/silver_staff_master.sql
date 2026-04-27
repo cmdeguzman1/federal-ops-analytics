1c4430b25bfca0d274472fbb9603c81ef381bfde
cmdeguzman1 <cmdeguzman1@gmail.com>
2026-04-27 17:39:24 -0500

    Updated sql command to include parsing out full name to first and last name.

---
+++ transformations/silver_staff_master.sql
@@ -0,0 +1,14 @@
+create or replace view staff_master as
+select
+    nullif(trim("Staff_ID"), '') as "staff_id",
+    nullif(trim("Full_Name"), '') as "full_name",
+    nullif(trim("Project_Name"), '') as "project_name",
+    nullif(trim("Department"), '') as "department",
+    nullif(trim("Job_Title"), '') as "job_title",
+    nullif(trim("Employee_Type"), '') as "employee_type",
+    nullif(trim("Employment_Status"), '') as "employment_status",
+    lower(nullif(trim("Email"), '')) as "email_address",
+    datediff('day', to_date(nullif(trim("Hire_Date"), '')), to_date(nullif(trim("Date_Left"), ''))) as "total_days_employed",
+    round(datediff('day', to_date(nullif(trim("Hire_Date"), '')), to_date(nullif(trim("Date_Left"), ''))) / 30, 1) as "approx_months_employed"
+
+from federal_ops.bronze.smartsheet_staff_master_v4;
